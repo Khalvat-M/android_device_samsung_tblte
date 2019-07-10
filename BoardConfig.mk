20c9c5a56e510eac7025545d722ff2a4601d1371
Khalvat-M <mmba313@gmail.com>
2019-07-10 13:58:20 +0430

    cleanup deprecate flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,15 +18,7 @@
 
 TARGET_OTA_ASSERT_DEVICE := tblte,tbltedt,tbltecan,tbltespr,tbltetmo,tblteusc,tbltevzw,tbltexx
 
-# Bootanimation
-TARGET_SCREEN_WIDTH := 1600
-
-# Camera
-TARGET_FIXUP_PREVIEW := true
-
 # Kernel
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3b7 dwc3_msm.cpu_to_affin=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_tblte_eur_defconfig
 
 # Init
